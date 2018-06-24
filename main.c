0b03717a400012250245521b5031b16a686ac4c9
studiosoftdev <40466018+studiosoftdev@users.noreply.github.com>
2018-06-24 02:57:49 +0100

    Editor
    added first signs of editor

--- main.c
+++ main.c
@@ -2,16 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-void getNewLine(char newLine[]);
+void addNewLine(char newLine[], int lineNum);
+void startFile();
+void closeFile();
 
 int main()
 {
-    char newLine[50];
+    printf("CLMCE Editor V0.1\n");
+    char newLine[25];
     int lineNum = 1;
+    startFile();
     do{
         printf("%d| ", lineNum);
         fgets(newLine, 25, stdin);
         lineNum++;
+        addNewLine(newLine, lineNum);
     } while (strcmp(newLine, "END\n") != 0);
+    //closeFile();
     return 0;
+}
+
+void addNewLine(char newLine[], int lineNum){
+    FILE *tempFile;
+    tempFile = fopen("temp.txt", "a");
+    fprintf(tempFile, "%d,%s", lineNum - 1, newLine);
+}
+
+void startFile(){
+    FILE *tempFile;
+    tempFile = fopen("temp.txt", "w");
+    fprintf(tempFile, "START\n");
+}
+
+void closeFile(){
+    FILE *tempFile;
+    tempFile = fopen("temp.txt", "a");
+    fprintf(tempFile, "enter important stuff you might need here");
+    fclose(tempFile);
 }
